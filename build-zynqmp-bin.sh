aeead77795d437512c370674e87e185243b72a6f
toystar4u <46470737+toystar4u@users.noreply.github.com>
2020-04-14 17:20:38 +0900

    Update build-zynqmp-bin.sh

--- build-zynqmp-bin.sh
+++ build-zynqmp-bin.sh
@@ -1,4 +1,11 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 bitstream.bit"
+  exit
+fi
+
 tmp_zynqmp_bif=`basename $1`.bif
-echo "all:{ [deistination_device = pl] $1}" >  $tmp_bitmp_zynqmp_bif
+echo "all:{ [deistination_device = pl] $1 }" >  $tmp_bitmp_zynqmp_bif
 bootgen -image  -arch zynqmp -o  $1.bin -w
 rm -rf $tmp_zynqmp_bif
