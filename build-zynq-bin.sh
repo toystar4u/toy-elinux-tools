44f8911e9b74c1edd6e206296bc95d1ff0764e47
toystar4u <46470737+toystar4u@users.noreply.github.com>
2020-04-14 19:03:01 +0900

    Update build-zynq-bin.sh

--- build-zynq-bin.sh
+++ build-zynq-bin.sh
@@ -7,5 +7,5 @@ fi
 
 tmp_zynq_bif=`basename $1`.bif
 echo "all:{ $1 }" >  $tmp_zynq_bif
-bootgen -image $tmp_zynq_bif -arch zynq -process_bitstream bin
+./bootgen -image $tmp_zynq_bif -arch zynq -process_bitstream bin
 rm -rf $tmp_zynq_bif
