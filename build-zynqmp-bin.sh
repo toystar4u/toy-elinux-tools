6d2fa949968ce886a8538d1d31637384ea25882e
toystar4u <46470737+toystar4u@users.noreply.github.com>
2020-04-14 19:03:33 +0900

    Update build-zynqmp-bin.sh

--- build-zynqmp-bin.sh
+++ build-zynqmp-bin.sh
@@ -7,5 +7,5 @@ fi
 
 tmp_zynqmp_bif=`basename $1`.bif
 echo "all:{ [deistination_device = pl] $1 }" >  $tmp_bitmp_zynqmp_bif
-bootgen -image  -arch zynqmp -o  $1.bin -w
+./bootgen -image  -arch zynqmp -o  $1.bin -w
 rm -rf $tmp_zynqmp_bif
