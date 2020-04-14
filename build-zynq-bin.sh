2b77c9becea9dc280322fb28aa3f3d42ad81320d
toystar4u <46470737+toystar4u@users.noreply.github.com>
2020-04-14 17:19:50 +0900

    Update build-zynq-bin.sh

--- build-zynq-bin.sh
+++ build-zynq-bin.sh
@@ -1,4 +1,11 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 bitstream.bit"
+  exit
+fi
+
 tmp_zynq_bif=`basename $1`.bif
-echo "all:{$1}" >  $tmp_zynq_bif
+echo "all:{ $1 }" >  $tmp_zynq_bif
 bootgen -image $tmp_zynq_bif -arch zynq -process_bitstream bin
 rm -rf $tmp_zynq_bif
